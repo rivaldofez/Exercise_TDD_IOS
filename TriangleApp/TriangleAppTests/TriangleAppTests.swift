b61f30025d810cb01def379bb8bcfdb73cb76ae4
rivaldofez <rivaldofez@gmail.com>
2023-03-02 12:45:02 +0700

    implement test for equilateral triangle

--- TriangleApp/TriangleAppTests/TriangleAppTests.swift
+++ TriangleApp/TriangleAppTests/TriangleAppTests.swift
@@ -15,10 +15,20 @@ class TriangleAppTests: XCTestCase {
         }
     }
     
-    func detectTriangle(_ sideA: Int, _ sideB: Int, _ sideC: Int) throws {
+    func detectTriangle(_ sideA: Int, _ sideB: Int, _ sideC: Int) throws -> String {
         for side in [sideA, sideB, sideC] where side < 1 {
             throw TriangleError.invalidInput
         }
+        
+        if sideA == sideB && sideA == sideC && sideB == sideC {
+            return "Segitiga Sama Sisi"
+        } else {
+            return "Bukan Segitiga Sama Sisi"
+        }
+    }
+    
+    func testDetectEquilateralTriangle() {
+        XCTAssertEqual(try detectTriangle(2, 2, 2), "Segitiga Sama Sisi")
     }
 }
 
